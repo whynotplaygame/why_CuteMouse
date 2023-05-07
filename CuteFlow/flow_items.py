eaa749c32e05d7bfd76abe4d81b0fa8509c420ae
why <vvwhyvv@163.com>
2023-05-07 23:43:39 +0800

    tijiao

---
+++ CuteFlow/flow_items.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    @Author wuhongyu
+    @Date 2023/5/7 11:38 PM
+    @Describe 
+    @Version 1.0
+"""
+from PySide6.QtWidgets import QGraphicsRectItem
+
+
+class FlowRectItem(QGraphicsRectItem):
+    def __init__(self):
+        super(FlowRectItem, self).__init__()
